7efc804387d48eb54a3d67c82caf1c16de73d5f3
Arnaud <89442525+Ed4ru@users.noreply.github.com>
2024-10-01 00:52:00 +0200

    refactor: better `palettes` format to reduce bundle size

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,18 +8,20 @@ export interface ColorShade {
 }
 
 export type ColorShades = [
-  string, // 50
-  string, // 100
-  string, // 200
-  string, // 300
-  string, // 400
-  string, // 500
-  string, // 600
-  string, // 700
-  string, // 800
-  string, // 900
-  string, // 950
+  string, // hex code for the shade 50
+  string, // ... 100
+  string, // ... 200
+  string, // ... 300
+  string, // ... 400
+  string, // ... 500
+  string, // ... 600
+  string, // ... 700
+  string, // ... 800
+  string, // ... 900
+  string, // ... 950
 ]
+
+export type BaseColorPalette = [string, ColorShades]
 
 export interface ColorPalette {
   name: string
